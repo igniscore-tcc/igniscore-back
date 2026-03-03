33720d1b418fa348810d869be1d1e76737849123
andersongama-dev <andersongamasilva5@gmail.com>
2026-03-03 00:31:21 -0300

    first commit

---
+++ src/main/java/com/igniscore/api/IgniscoreApplication.java
@@ -0,0 +1,13 @@
+package com.igniscore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igniscore/api/IgniscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igniscore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
