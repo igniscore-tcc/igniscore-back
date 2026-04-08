397999651a6025ea4c0db6e4226aa5b5accdedd2
andersongama-dev <andersongamasilva5@gmail.com>
2026-04-08 14:08:55 -0300

    fix: adjust service, controller, and client model
    - fix issues in Client model
    - add proper service call in controller
    - refine client creation flow
    - prepare structure for future sequential code implementation

--- src/main/java/com/igniscore/api/controller/ClientController.java
+++ src/main/java/com/igniscore/api/controller/ClientController.java
@@ -1,13 +1,34 @@
-//package com.igniscore.api.controller;
-//
-//import org.apache.coyote.Response;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//
-//import java.awt.geom.RectangularShape;
-//
-//public class ClientController {
-//    @PostMapping("/clients")
-//    public ResponseEntity<String> register(@RequestBody )
-//}
+package com.igniscore.api.controller;
+
+import com.igniscore.api.model.Client;
+import com.igniscore.api.service.ClientService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ClientController {
+
+    private final ClientService service;
+
+
+    public ClientController(ClientService service) {
+        this.service = service;
+    }
+
+    @MutationMapping
+    public Client createClient(@Argument String name,
+                               @Argument String cnpj,
+                               @Argument String email,
+                               @Argument Integer number,
+                               @Argument String ie,
+                               @Argument String uf_ie,
+                               @Argument String obs) {
+        return service.createClient(name, cnpj, email, number, ie, uf_ie, obs);
+    }
+
+
+    //@PostMapping("/clients")
+    //public ResponseEntity<String> register(@RequestBody )
+}
