aca4f6b0d31775a02c264db6905003fb1ac751b4
andersongama-dev <andersongamasilva5@gmail.com>
2026-06-08 03:36:35 -0300

    feat: creating a unit test for findAll clients

--- src/test/java/com/igniscore/api/service/ClientServiceTest.java
+++ src/test/java/com/igniscore/api/service/ClientServiceTest.java
@@ -2,6 +2,7 @@ package com.igniscore.api.service;
 
 import com.igniscore.api.dto.client.ClientRegisterDTO;
 import com.igniscore.api.dto.company.CreateCompanyDTO;
+import com.igniscore.api.model.Client;
 import com.igniscore.api.model.Company;
 import com.igniscore.api.model.User;
 import com.igniscore.api.repository.ClientRepository;
@@ -15,9 +16,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -84,5 +89,78 @@ class ClientServiceTest {
         assertNotNull(client);
 
         assertEquals(company, client.getCompany());
+    }
+
+    @Test
+    @DisplayName("Should must successfully list all clients")
+    void shouldFindAllClient() {
+
+        CreateCompanyDTO dtoCompany = new CreateCompanyDTO(
+                "IgnisCore",
+                "71.963.415/0001-09",
+                "572.754.780.502",
+                "SP",
+                "suporte@igniscore.com",
+                "1935798593"
+        );
+
+        var company = new Company(dtoCompany);
+
+        User user = new User(1);
+        user.setCompany(company);
+
+        given(authUserService.getUserOrThrow()).willReturn(user);
+
+        given(authUserService.getCompanyOrThrow()).willReturn(company);
+
+        var dtoClientOne = new ClientRegisterDTO(
+                "Cliente do IgnisCore",
+                "clienteignscore@gmail.com",
+                "37201849000133",
+                "99999999999",
+                "411873849804",
+                "SP",
+                ""
+        );
+
+        var dtoClientTwo = new ClientRegisterDTO(
+                "Cliente do IgnisCore",
+                "clienteignscore@gmail.com",
+                "37201849000133",
+                "99999999999",
+                "411873849804",
+                "SP",
+                ""
+        );
+
+        given(clientRepository.save(any()))
+                .willAnswer(invocation -> invocation.getArgument(0));
+
+        var clientOne = clientService.store(dtoClientOne);
+        var clienteTwo = clientService.store(dtoClientTwo);
+
+        int page = 0;
+        int size = 10;
+
+        Pageable pageable = PageRequest.of(
+                page,
+                size,
+                Sort.by(Sort.Direction.ASC, "id")
+        );
+
+        List<Client> clients = List.of(clientOne, clienteTwo);
+
+        Page<Client> pageResult =
+                new PageImpl<>(clients, pageable, clients.size());
+
+        given(clientRepository.findByCompany(
+                eq(company),
+                eq(pageable)
+        )).willReturn(pageResult);
+
+        var clientes = clientService.findAll(pageable);
+
+        assertNotNull(clientes);
+        assertEquals(2, clientes.getClients().size());
     }
 }
