efe28a590bb5295619fc3fac8bbd3b7200e4046a
GabrielBenedetti09 <gabriel.bene.oliveira@gmail.com>
2026-06-12 18:21:24 -0300

    test:  ProductRepository has been well tested so far.

---
+++ src/test/java/com/igniscore/api/service/ProductServiceTest.java
@@ -0,0 +1,9 @@
+package com.igniscore.api.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+}
---
+++ src/test/java/com/igniscore/api/service/SaleServiceTest.java
@@ -0,0 +1,7 @@
+package com.igniscore.api.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaleServiceTest {
+
+}
